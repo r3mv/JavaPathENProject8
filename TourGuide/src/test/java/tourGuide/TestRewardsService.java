fedfa992d1c9accbf1f3539b0eff90da8a0a49bd
remi <remi.vieux@gmail.com>
2023-07-26 16:39:49 +0200

    upd:TestRewardsService/Fix probleme de locale à l'init

--- TourGuide/src/test/java/tourGuide/TestRewardsService.java
+++ TourGuide/src/test/java/tourGuide/TestRewardsService.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -20,6 +22,11 @@ import tourGuide.user.User;
 import tourGuide.user.UserReward;
 
 public class TestRewardsService {
+
+	@BeforeClass
+	public static void init () {
+		Locale.setDefault(Locale.US);
+	}
 
 	@Test
 	public void userGetRewards() {
